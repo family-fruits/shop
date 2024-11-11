a0a3898793126ff14b13e53259f270d26379bc55
Vlad <vladys.s.1990@gmail.com>
2024-11-11 23:22:01 +0200

    Updated list of fruits

--- js/products/fruits.js
+++ js/products/fruits.js
@@ -1,65 +1,240 @@
 const fruitsData = [
     {
         id: 1,
-        name: 'Апельсин',
+        name: 'Авокадо',
         category: "fruits",
-        minOrder: '0.5 кг',
-        available: false
+        minOrder: '1 шт',
+        available: true
     },
     {
         id: 2,
-        name: 'Гранат',
-        category: "fruits",
-        minOrder: '0.5 кг',
-        available: true
-    },
-    {
-        id: 3,
-        name: 'Лайм',
-        category: "fruits",
-        minOrder: '0.3 кг',
-        available: true
-    },
-    {
-        id: 4,
-        name: 'Грейпфрут',
-        category: "fruits",
-        minOrder: '0.3 кг',
-        available: false
-    },
-    {
-        id: 5,
-        name: 'Лимон',
-        category: "fruits",
-        minOrder: '0.5 кг',
-        available: true
-    },
-    {
-        id: 6,
-        name: 'Нектарин',
-        category: "fruits",
-        minOrder: '0.5 кг',
-        available: true
-    },
-    {
-        id: 7,
-        name: 'Персик Україна',
-        category: "fruits",
-        minOrder: '0.5 кг',
-        available: true
-    },
-    {
-        id: 8,
         name: 'Авокадо Хасс',
         category: "fruits",
         minOrder: '1 шт',
         available: true
     },
     {
+        id: 3,
+        name: 'Ананас А6',
+        category: "fruits",
+        minOrder: '1 шт',
+        available: false
+    },
+    {
+        id: 4,
+        name: 'Ананас Голд',
+        category: "fruits",
+        minOrder: '1 шт',
+        available: false
+    },
+    {
+        id: 5,
+        name: 'Апельсин добірний',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 6,
+        name: 'Апельсин фреш',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 7,
+        name: 'Банан',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 8,
+        name: 'Гранат',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
         id: 9,
+        name: 'Грейпфрут',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 10,
+        name: 'Груша Конференція Імпорт',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 11,
+        name: 'Груша Ксенія Імпорт',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 12,
+        name: 'Груша Туреччина',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 13,
         name: 'Груша Україна',
         category: "fruits",
         minOrder: '0.5 кг',
         available: true
+    },
+    {
+        id: 14,
+        name: 'Помело червоне',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 15,
+        name: 'Ківі',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 16,
+        name: 'Лайм',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 17,
+        name: 'Мандарин',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 18,
+        name: 'Мандарин клементин',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 19,
+        name: 'Мандарин преміум',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 20,
+        name: 'Помело',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 21,
+        name: 'Світі',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 22,
+        name: 'Слива Імпорт',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 23,
+        name: 'Слива угорка',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 24,
+        name: 'Фейхоа Грузія',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 25,
+        name: 'Хурма Азербайджан',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 26,
+        name: 'Хурма Шарон',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 27,
+        name: 'Яблуко Голден',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 28,
+        name: 'Яблуко Горець',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 29,
+        name: 'Яблуко Муцу',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 30,
+        name: 'Яблуко Ред Чіф',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 31,
+        name: 'Яблуко Симиренко',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 32,
+        name: 'Яблуко Флоріна',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
+    },
+    {
+        id: 33,
+        name: 'Яблуко Чемпіон',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: true
+    },
+    {
+        id: 34,
+        name: 'Яблуко Чорний принц',
+        category: "fruits",
+        minOrder: '0.5 кг',
+        available: false
     },
 ];
